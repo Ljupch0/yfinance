f95def2410ba7027fae275a70791d3acabf69a88
Ljupcho <lj.naumov@gmail.com>
2021-01-10 18:50:49 +0100

    more crayon fixes

--- R/safe_download.R
+++ R/safe_download.R
@@ -4,7 +4,8 @@
 #' @param proto_function A function that needs to run safely and not stop at errors.
 #' @param report_type Either "quarterly" or "annual", depending whether the proto function accepts such an argument.
 #'
-#' @import crayon
+#' @importFrom crayon red
+#' @importFrom crayon cyan
 #'
 #' @return Dataframe with errors as attributes.
 safe_download <- function(ticker, report_type, proto_function) {
@@ -22,6 +23,8 @@ safe_download <- function(ticker, report_type, proto_function) {
 
 
   if (length(error_tickers) != 0) {
+    red <- crayon::red()
+    cyan <- crayon::cyan()
     warning(glue::glue_col("There were {red ERRORS} while downloading data for these tickers: {cyan {paste(error_tickers, collapse = ', ')}.}
                        Please check if you have the correct ticker. The vector of errored tickers and error messages can be accessed as attributes of the returned dataframe - attributes(df)."))
   }
