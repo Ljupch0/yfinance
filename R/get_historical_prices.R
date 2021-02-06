781610c650a99955140d66f9bda8eaa091c48755
Ljupcho <lj.naumov@gmail.com>
2021-02-06 20:26:12 +0100

    Added 2 new functions: get_historical_price and get_company_names

---
+++ R/get_historical_prices.R
@@ -0,0 +1,19 @@
+get_historical_price <- function(ticker) {
+  pb <- progress_bar(datatype = "Prices", ticker = ticker)
+  get_price_proto <- function(ticker, ...) {
+    pb$tick(tokens = list(what = ticker))
+    jsonlite::flatten(
+      jsonlite::fromJSON(
+        glue::glue("https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price")
+      )[[1]][[1]][[1]]
+    ) %>%
+      select(ends_with(".raw")) %>%
+      `names<-`(sub(".raw","", names(.))) %>%
+      mutate(
+        ticker = ticker,
+        date = Sys.Date()
+      ) %>%
+      select(ticker, date, everything())
+  }
+  safe_download(ticker = ticker, proto_function = get_price_proto)
+}
